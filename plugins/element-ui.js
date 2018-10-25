23aa04cd8afeceb445bf201fef2d247b300072de
List412 <erestor412@gmail.com>
2018-10-25 23:02:46 +0500

    little size optimization

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,26 +1,8 @@
 import Vue from "vue";
 
-import { Input } from 'element-ui/lib/input'
-import { Table } from 'element-ui/lib/table'
-import { Switch } from 'element-ui/lib/switch'
-import { DatePicker } from 'element-ui/lib/date-picker'
-import { Card } from 'element-ui/lib/card'
-
-// import ElementUI from 'element-ui'
+import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ru-RU'
 
-Vue.use(Input, {
-  locale
-})
-Vue.use(Table, {
-  locale
-})
-Vue.use(Switch, {
-  locale
-})
-Vue.use(DatePicker, {
-  locale
-})
-Vue.use(Card, {
+Vue.use(ElementUI, {
   locale
 })

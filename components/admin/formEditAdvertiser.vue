9a907616de489024af1818441a69b1566954d84d
Mikhail Sergeev <ima1ta1r@yandex.ru>
2018-11-04 01:09:49 +0500

    Added username/email exists validation

--- components/admin/formEditAdvertiser.vue
+++ components/admin/formEditAdvertiser.vue
@@ -28,23 +28,28 @@
 </template>
 
 <script>
+  import {mapState} from 'vuex'
+
   export default {
     name: "formEditAdvertiser",
     data() {
       return {
         initialUsername: this.advertiser.username,
+        initialEmail: this.advertiser.email,
         rules: {
           username: [
             {required: true, message: 'Пожалуйста, введите название нового рекламодателя', trigger: 'blur'},
-            {min: 3, message: 'Длина имени должна быть больше 3 символов', trigger: 'blur'}
+            {min: 3, message: 'Длина имени должна быть больше 3 символов', trigger: 'change'},
+            {validator: this.usernameValidator, trigger: 'blur'}
           ],
           password: [
             {required: true, message: 'Пожалуйста, введите пароль для рекламодателя', trigger: 'blur'},
-            {min: 3, message: 'Длина должна быть больше 3 символов', trigger: 'blur'}
+            {min: 3, message: 'Длина должна быть больше 3 символов', trigger: 'change'}
           ],
           email: [
-            {required: true, message: 'Пожалуйста, введите адрес электронной почты', trigger: 'change'},
-            {type: 'email', message: 'Пожалуйста, введите корректный адрес электронной почты', trigger: 'blur'}
+            {required: true, message: 'Пожалуйста, введите адрес электронной почты', trigger: 'blue'},
+            {type: 'email', message: 'Пожалуйста, введите корректный адрес электронной почты', trigger: 'blur'},
+            {validator: this.emailValidator, trigger: 'blur'}
           ],
         }
       }
@@ -59,16 +64,38 @@
         notes: '',
       },
     },
+    mounted() {
+      if (this.$store.state.advertisers.length === 0) {
+        this.$store.dispatch('load_advertisers')
+      }
+    },
+    computed: {
+      ...mapState(['advertisers'])
+    },
     methods: {
+      usernameValidator(rule, value, callback) {
+        if (this.advertisers.findIndex(x => x.username === value && value !== this.initialUsername) !== -1) {
+          callback(new Error('Рекламодатель с таким названием уже существует'));
+        }
+        callback()
+      },
+      emailValidator(rule, value, callback) {
+        if (this.advertisers.findIndex(x => x.email === value && value !== this.initialEmail) !== -1) {
+          callback(
+            new Error('Рекламодатель с таким email уже существует'))
+        }
+        callback();
+      },
       submitForm(formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
             this.$store.dispatch('update_advertiser_total', this.advertiser)
-              .then(this.notify())
-              .then(this.$router.back(1))
-          } else {
-            console.log('error submit!!');
-            return false;
+              .then(() => {
+                this.$router.push('/secure/admin')
+                this.notify()
+              }).catch(() => {
+              this.notifyFail()
+            })
           }
         });
       },
@@ -76,9 +103,16 @@
         this.$router.push('/secure/admin')
       },
       notify() {
-        this.$notify({
+        this.$notify.success({
           title: `Обновлено`,
           message: `Данные рекламодателя ${this.advertiser.username} успешно обновлены`,
+          position: 'bottom-right'
+        });
+      },
+      notifyFail() {
+        this.$notify.error({
+          title: `Рекламодатель ${this.advertiser.username} не был обновлен`,
+          message: `При обновлении рекламодателя произошла ошибка. Проверьте, что введенные данные верны`,
           position: 'bottom-right'
         });
       }
